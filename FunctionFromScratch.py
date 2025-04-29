2b0377c6178c49dfef24514d4e6ab3a2cf8634f0
drngominhtu <dr.ngominhtu@gmail.com>
2025-04-29 11:59:39 +0700

    refix log transform

--- FunctionFromScratch.py
+++ FunctionFromScratch.py
@@ -95,7 +95,23 @@ def rootfunc(img, index):
     return matrixzeros
 
 
-
+def logtransformfunc4(img, min_val, max_val, c):
+    height, width = img.shape[:2]
+    matrixzeros = np.zeros((height, width), dtype=np.uint8)
+    
+    for i in range(height):
+        for j in range(width):
+            singlepixel = img[i, j]
+            if singlepixel < min_val:
+                matrixzeros[i, j] = singlepixel  
+            elif singlepixel > max_val:
+                matrixzeros[i, j] = singlepixel  
+            else:             
+                normalized = (singlepixel - min_val) / (max_val - min_val)  
+                newpixel = c * math.log(1 + normalized) * (max_val - min_val) + min_val
+                matrixzeros[i, j] = np.clip(newpixel, 0, 255)
+    
+    return matrixzeros
 
 newimglog = logtransformfunc(img, 90)
 newimgThreshold = thresholdfunc(img, 127)
@@ -104,15 +120,24 @@ newimgnegative = negativefunc(img)
 newimgpower = powerfunc(img, 4)
 newimgroot = rootfunc(img, 2)
 newimglog2 = logtransformfunc2(img, 100, 200)
+newimglog3 = cv.cvtColor(newimglog2, cv.COLOR_GRAY2RGB)
+newimglog4 = logtransformfunc4(img, 50, 100, -10)
+
 
 
 cv.imshow("Original img", img)
-cv.imshow("after log transform", newimglog)
-cv.imshow("after threshold", newimgThreshold)
-cv.imshow("after identity", newimgIdentity)
-cv.imshow ("after negative", newimgnegative)
-cv.imshow("after power", newimgpower)
-cv.imshow("after root", newimgroot)
-cv.imshow("after log2", newimglog2)
+# cv.imshow("after log transform", newimglog)
+# cv.imshow("after threshold", newimgThreshold)
+# cv.imshow("after identity", newimgIdentity)
+# cv.imshow ("after negative", newimgnegative)
+# cv.imshow("after power", newimgpower)
+# cv.imshow("after root", newimgroot)
+# cv.imshow("after log2", newimglog2)
+# cv.imshow("after log3", newimglog3)
+cv.imshow("after log4", newimglog4)
+
+
+
+
 
 cv.waitKey(0)
